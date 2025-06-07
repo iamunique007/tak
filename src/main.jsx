0642a74174d050498b1208bfc6f2ed761324b0da
HP <you@example.com>
2025-06-07 20:05:28 +0500

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx'
 import './App.css'
 
 createRoot( document.getElementById('root') ).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/tak'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
