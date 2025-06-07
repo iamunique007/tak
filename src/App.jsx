c1f7e9a781409db6efa0000e742f12c77777f786
HP <you@example.com>
2025-06-07 19:41:38 +0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Taklifnoma from './views/taklifnoma'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/:name' element={ <Taklifnoma /> }></Route>
-    </Routes>
+      <Routes>
+        <Route path='/kim' element={<div> Ishladi </div>}/>
+        <Route path='/:name' element={ <Taklifnoma /> }></Route>
+      </Routes>
   )
 }
 
